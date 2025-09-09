815ce53ed8b8e5e25107be6bcf9d0d0a2dc985cf
worldvit <worldvit@github.com>
2025-09-09 16:24:40 +0900

    update userdata

--- instances.tf
+++ instances.tf
@@ -1,12 +1,12 @@
 // ec2-web01
 // template file for WEB server
-data "template_file" "user_data_01" {
-  template = templatefile("script-nginx-01.sh", {})
-}
+# data "template_file" "user_data_01" {
+#   template = templatefile("script-nginx-01.sh", {})
+# }
 
-data "template_file" "user_data_02" {
-  template = templatefile("script-nginx-02.sh", {})
-}
+# data "template_file" "user_data_02" {
+#   template = templatefile("script-nginx-02.sh", {})
+# }
 
 ## ec2-WEB
 resource "aws_instance" "ec2-web01" {
@@ -16,9 +16,8 @@ resource "aws_instance" "ec2-web01" {
   subnet_id       = aws_subnet.itskillboost-private-subnet-01.id
   associate_public_ip_address = false
   key_name = aws_key_pair.mykey.key_name
-  user_data = data.template_file.user_data_01.rendered
-
-
+#   user_data = data.template_file.user_data_01.rendered
+  user_data_base64 = filebase64("script-nginx-01.sh")
   root_block_device {
     volume_size = "8"
     volume_type = "gp3"
@@ -26,6 +25,7 @@ resource "aws_instance" "ec2-web01" {
       "Name" = "ec2-web01"
     }
   }
+
   tags = {
     "Name" = "ec2-web01"
   }
@@ -40,8 +40,8 @@ resource "aws_instance" "ec2-web02" {
   subnet_id       = aws_subnet.itskillboost-private-subnet-02.id
   associate_public_ip_address = false
   key_name = aws_key_pair.mykey.key_name
-  user_data = data.template_file.user_data_02.rendered
-
+#   user_data = data.template_file.user_data_02.rendered
+  user_data_base64 = filebase64("script-nginx-02.sh")
 
   root_block_device {
     volume_size = "8"
